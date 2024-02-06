00f690f87e269765fc8300825a1e57b3ec292074
verity <80880870+verity0001@users.noreply.github.com>
2024-02-06 23:59:11 +0100

    Minor changes, optimized the code

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 window.onload = hidenav()
-
+var x = window.matchMedia("(max-width: 700px)")
 let nav = false
 
+window.addEventListener("resize", () => {
+    console.log(window.innerWidth);
+    toggleNav();
+});
 function toggleNav() {
-    if (nav == false) {
-        nav = true
-        document.getElementById("smenu").style.display = "inline";
-    } else {
-        nav = false
-        document.getElementById("smenu").style.display = "none";
+    if (x.matches) {
+        if (nav == false) {
+            nav = true
+            document.getElementById("smenu").style.display = "inline";
+        } else {
+            nav = false
+            document.getElementById("smenu").style.display = "none";
+        }
     }
 }
 
